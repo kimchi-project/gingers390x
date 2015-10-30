ab09f6bb91fc4fb0ee6a9793bdd02da78190240d
Archana Singh <archus@linux.vnet.ibm.com>
2015-10-31 02:18:08 +0530

    Ginger S390x: Added mockmodel.py

---
+++ mockmodel.py
@@ -0,0 +1,32 @@
+#
+# Project Ginger S390x
+#
+# Copyright IBM, Corp. 2015
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+from wok.plugins.gingers390x.model.model import Model
+
+
+fake_user = {'root': 'letmein!'}
+mockmodel_defaults = {'domain': 'test', 'arch': 's390x'}
+
+
+class MockModel(Model):
+
+    def __init__(self, objstore_loc=None):
+        # Override osinfo.defaults to ajust the values according to
+        # test:///default driver
+        pass
